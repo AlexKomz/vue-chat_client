d068f5d6cbd36f84410067a946d5785d8be081ad
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-11-19 12:26:47 +0300

    Вынес облачка сообщений в отдельный компонент

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,69 @@
+<template>
+  <li
+    class="chat-item"
+    :key="message.id"
+    :class="{ 'chat-item_self': message.isSelf }"
+  >
+    {{ message.text }}
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ChatMessage",
+  props: {
+    message: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chat-item {
+  position: relative;
+
+  padding: 10px;
+
+  max-width: 50%;
+
+  border-radius: 10px;
+
+  background-color: var(--green-color);
+  color: var(--main-color);
+}
+
+.chat-item:before {
+  content: "";
+
+  position: absolute;
+
+  top: -2px;
+
+  display: block;
+
+  width: 10px;
+  height: 10px;
+
+  background-color: inherit;
+}
+
+.chat-item:not(.chat-item_self):before {
+  left: 4px;
+
+  transform: rotate(30deg);
+}
+
+.chat-item_self {
+  align-self: flex-end;
+
+  background-color: var(--light-green-color);
+}
+
+.chat-item_self:before {
+  right: 4px;
+
+  transform: rotate(60deg);
+}
+</style>
