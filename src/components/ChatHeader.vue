ae747f2d8a77260cd9a4a348af919f7540a20f58
Aleksandr <a.komzoltsev@gmail.com>
2021-12-05 15:43:16 +0300

    Добавил кнопку настроек на будущее

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -1,20 +1,29 @@
 <template>
   <nav class="header">
+    <ButtonWithIcon class="header__settings">
+      <SettingsSVG class="settings__icon" />
+    </ButtonWithIcon>
     <a href="#" class="header__wrapper">
-      <span class="header__dark">Super</span>
+      <span class="header__dark">Simple</span>
       <span class="header__light">Chat</span>
     </a>
   </nav>
 </template>
 
 <script>
+import ButtonWithIcon from "@/components/ButtonWithIcon";
+import SettingsSVG from "../assets/settings-icon.svg";
+
 export default {
   name: "ChatHeader",
+  components: { ButtonWithIcon, SettingsSVG },
 };
 </script>
 
 <style scoped>
 .header {
+  position: relative;
+
   min-height: 100px;
 
   display: flex;
@@ -33,13 +42,34 @@ export default {
   text-decoration: none;
   font-size: 76px;
   font-weight: bold;
+  user-select: none;
 }
 
 .header__dark {
-  color: var(--green-color);
+  color: var(--companion-message-bg-color);
 }
 
 .header__light {
-  color: var(--light-green-color);
+  color: var(--message-bg-color);
+}
+
+.header__settings {
+  position: absolute;
+  top: 6px;
+  right: 6px;
+
+  transition: transform 0.4s;
+}
+
+.header__settings:hover,
+.header__settings:focus {
+  transform: rotate(90deg);
+}
+
+.settings__icon {
+  width: 20px;
+  height: 20px;
+
+  fill: var(--black-color);
 }
 </style>
