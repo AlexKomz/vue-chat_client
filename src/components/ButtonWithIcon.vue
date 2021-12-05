26f7c90a104c4a893fb74f779c893ca92eb9c8d5
Aleksandr <a.komzoltsev@gmail.com>
2021-12-05 15:41:37 +0300

    Создал новый компонент для кнопки с иконкой внутри

---
+++ src/components/ButtonWithIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonWithIcon",
+};
+</script>
+
+<style scoped>
+.button {
+  display: flex;
+  padding: 0;
+
+  border: none;
+  outline: none;
+  background: transparent;
+  cursor: pointer;
+}
+
+.button:hover,
+.button:focus {
+  opacity: 0.6;
+}
+
+.button:active {
+  opacity: 0.3;
+}
+</style>
