cab6be5f616776153d2eabebf171308c50a5cec9
Aleksandr <a.komzoltsev@gmail.com>
2021-12-08 21:57:31 +0300

    Роут с лоадером для поиска

---
+++ src/views/SearchView.vue
@@ -0,0 +1,36 @@
+<template>
+  <BackgroundWrapper>
+    <div class="search">
+      <LoaderIcon />
+    </div>
+  </BackgroundWrapper>
+</template>
+
+<script>
+import BackgroundWrapper from "@/wrappers/BackgroundWrapper";
+import LoaderIcon from "@/components/LoaderIcon";
+
+import { ROOT, CHAT } from "@/router/pathes";
+
+export default {
+  name: "SearchView",
+  components: { LoaderIcon, BackgroundWrapper },
+  beforeRouteEnter(to, from, next) {
+    if (from.path !== ROOT) next(ROOT);
+    next();
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$router.push(CHAT);
+    }, 1000);
+  },
+};
+</script>
+
+<style scoped>
+.search {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
