940a329e782326baf3861faf78f5c9e5a24e0725
Aleksandr <a.komzoltsev@gmail.com>
2021-12-08 20:17:45 +0300

    Заменил кнопку и добавил роут хук на переход

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -6,24 +6,30 @@
       :isPrinting="isPrinting"
     />
     <MessageArea :message.sync="message" @submit="submitHandler" />
-    <SubmitButton @click.native="submitHandler" />
+    <ChatButton class="chat-view__submit-button" @click.native="submitHandler">
+      &#10149;
+    </ChatButton>
   </div>
 </template>
 
 <script>
 import ChatWindow from "@/components/ChatWindow";
 import MessageArea from "@/components/MessageArea";
-import SubmitButton from "@/components/SubmitButton";
+import ChatButton from "@/components/ChatButton";
 
 export default {
   name: "ChatView",
-  components: { SubmitButton, MessageArea, ChatWindow },
+  components: { ChatButton, MessageArea, ChatWindow },
   data: () => ({
     message: "",
     isPrinting: false,
     messages: [],
     companionID: null,
   }),
+  beforeRouteLeave(to, from, next) {
+    const MESSAGE = "Do you really want to leave? The chat may be interrupted!";
+    this.beforeTabChange(MESSAGE, next);
+  },
   methods: {
     submitHandler() {
       if (!this.message) return;
@@ -34,16 +40,20 @@ export default {
       });
       this.message = "";
     },
+    beforeTabChange(message, next) {
+      const answer = window.confirm(message);
+      if (answer) {
+        next();
+      } else {
+        next(false);
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
 .chat-view {
-  flex-grow: 1;
-
-  height: 100%;
-
   display: grid;
   grid-template: 1fr 100px / 1fr 75px;
   gap: 10px;
@@ -51,5 +61,11 @@ export default {
 
 .chat-view__window {
   grid-column: span 2;
+}
+
+.chat-view__submit-button {
+  font-size: 30px;
+
+  transform: scale(-1, 1);
 }
 </style>
