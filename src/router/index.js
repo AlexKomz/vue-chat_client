5186d01732138fac5b4074c91e7b7948e28aace4
Aleksandr <a.komzoltsev@gmail.com>
2021-12-08 20:16:11 +0300

    Добавил главное меню и редирект на все неизвестные пути

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import MenuView from "../views/MenuView";
 import ChatView from "../views/ChatView";
 
 Vue.use(Router);
@@ -8,8 +9,16 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
+      path: "/",
+      component: MenuView,
+    },
+    {
       path: "/chat",
       component: ChatView,
+    },
+    {
+      path: "*",
+      redirect: "/",
     },
   ],
 });
