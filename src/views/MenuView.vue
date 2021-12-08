fc883eb17601ba3f54a92a1fe119dc1b37bf1324
Aleksandr <a.komzoltsev@gmail.com>
2021-12-08 20:15:38 +0300

    Добавил роут для главного меню

---
+++ src/views/MenuView.vue
@@ -0,0 +1,44 @@
+<template>
+  <BackgroundWrapper>
+    <div class="menu">
+      <ChatButton class="menu__start-button">
+        <RouterLink class="menu__link" to="/chat" exact>
+          Start chatting!
+        </RouterLink>
+      </ChatButton>
+    </div>
+  </BackgroundWrapper>
+</template>
+
+<script>
+import BackgroundWrapper from "@/wrappers/BackgroundWrapper";
+import ChatButton from "@/components/ChatButton";
+
+export default {
+  name: "MenuView",
+  components: { ChatButton, BackgroundWrapper },
+};
+</script>
+
+<style scoped>
+.menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.menu__start-button {
+  padding: 16px 20px;
+
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 26px;
+  text-transform: uppercase;
+}
+
+.menu__link {
+  text-decoration: none;
+  color: inherit;
+  font: inherit;
+}
+</style>
