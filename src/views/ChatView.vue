87034289b940736f1e843b8ca896ed72a2cb8262
Aleksandr <a.komzoltsev@gmail.com>
2021-12-08 17:25:53 +0300

    Убрал моки

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -11,61 +11,6 @@ import ChatWindow from "@/components/ChatWindow";
 import MessageArea from "@/components/MessageArea";
 import SubmitButton from "@/components/SubmitButton";
 
-const mock = {
-  messages: [
-    {
-      id: "0",
-      isSelf: true,
-      text: "Lorem ipsum dolor sit amet.",
-    },
-    {
-      id: "1",
-      isSelf: false,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-    {
-      id: "2",
-      isSelf: true,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-    {
-      id: "3",
-      isSelf: false,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-    {
-      id: "4",
-      isSelf: true,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-    {
-      id: "5",
-      isSelf: false,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-    {
-      id: "6",
-      isSelf: true,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-    {
-      id: "7",
-      isSelf: false,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-    {
-      id: "8",
-      isSelf: true,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-    {
-      id: "9",
-      isSelf: false,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, necessitatibus?",
-    },
-  ],
-};
-
 export default {
   name: "ChatView",
   components: { SubmitButton, MessageArea, ChatWindow },
@@ -73,19 +18,16 @@ export default {
     message: "",
     isPrinting: false,
     messages: [],
+    companionID: null,
   }),
-  created() {
-    this.messages = mock.messages;
-  },
   methods: {
     submitHandler() {
       if (!this.message) return;
-      console.log(this.message);
-      // this.messages.push({
-      //   id: Date.now(),
-      //   isSelf: true,
-      //   text: this.message,
-      // });
+      this.messages.push({
+        id: Date.now(),
+        isSelf: true,
+        text: this.message,
+      });
       this.message = "";
     },
   },
