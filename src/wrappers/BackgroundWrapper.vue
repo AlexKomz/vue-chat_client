ae3fc7c1dc29fd19ec9b591a7c40f455f05a4a6a
Aleksandr <a.komzoltsev@gmail.com>
2021-12-08 18:04:30 +0300

    Создал обертку, куда вынес одинаковую стилизацию

---
+++ src/wrappers/BackgroundWrapper.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="background-wrapper">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackgroundWrapper",
+};
+</script>
+
+<style scoped>
+.background-wrapper {
+  display: flex;
+
+  border-radius: 10px;
+
+  box-shadow: 0 0 6px 0 var(--black-color);
+  background-color: var(--side-bg-color);
+}
+
+.background-wrapper > * {
+  flex-grow: 1;
+}
+</style>
