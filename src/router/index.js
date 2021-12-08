b33b8390af596f07112333b0ff27fe9451389b1a
Aleksandr <a.komzoltsev@gmail.com>
2021-12-08 20:25:23 +0300

    Алиасы вместо полных путей

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import MenuView from "../views/MenuView";
-import ChatView from "../views/ChatView";
+import MenuView from "@/views/MenuView";
+import ChatView from "@/views/ChatView";
 
 Vue.use(Router);
 
