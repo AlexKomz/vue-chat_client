c0c551c9166e02ed5e70fe6ed9c0f7a21a6b90c8
Aleksandr <a.komzoltsev@gmail.com>
2021-12-08 21:54:34 +0300

    Использую константы в путях

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,31 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import { ROOT, SEARCH, CHAT, OTHER } from "./pathes";
+
 import MenuView from "@/views/MenuView";
 import ChatView from "@/views/ChatView";
+import SearchView from "@/views/SearchView";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
+      path: ROOT,
       component: MenuView,
     },
     {
-      path: "/chat",
+      path: SEARCH,
+      component: SearchView,
+    },
+    {
+      path: CHAT,
       component: ChatView,
     },
     {
-      path: "*",
-      redirect: "/",
+      path: OTHER,
+      redirect: ROOT,
     },
   ],
 });
