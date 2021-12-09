155dd8bcf7a0265e2d9bc68efe521c143ed2a9dc
Aleksandr <a.komzoltsev@gmail.com>
2021-12-09 20:46:36 +0300

    Добавил заготовку для страницы с настройками

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <BackgroundWrapper>
+    <div class="settings">
+      <span>Settings menu in progress</span>
+    </div>
+  </BackgroundWrapper>
+</template>
+
+<script>
+import BackgroundWrapper from "../wrappers/BackgroundWrapper";
+export default {
+  name: "SettingsView",
+  components: { BackgroundWrapper },
+};
+</script>
+
+<style scoped>
+.settings {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
